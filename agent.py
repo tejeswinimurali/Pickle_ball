fdce907157166f248f1c0dd61ca76939301608aa
Tejeswini M <tejeswini.m@prodapt.com>
2025-12-20 23:16:43 +0530

    Adjust LangChain imports to top-level agents module

--- agent.py
+++ agent.py
@@ -4,9 +4,10 @@ from typing import Dict
 
 from langchain.tools import tool
 from langchain_openai import ChatOpenAI
-from langchain.agents import AgentExecutor
-from langchain.agents.tool_calling_agent.base import create_tool_calling_agent
+from langchain.agents import AgentExecutor, create_tool_calling_agent
 from langchain_core.prompts import ChatPromptTemplate
+
+
 
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
